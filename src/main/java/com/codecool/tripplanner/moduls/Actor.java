9bb0efb64970210f8dd8269f7a309e292903aecc
davidlenner <david910128@gmail.com>
2018-10-16 14:53:50 +0200

    packages and classes are created

--- src/main/java/com/codecool/tripplanner/moduls/Actor.java
+++ src/main/java/com/codecool/tripplanner/moduls/Actor.java
@@ -1,6 +1,11 @@
 package com.codecool.tripplanner.moduls;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Actor {
 
+    private String name;
+    List<Movie> movieList = new ArrayList<>();
 
 }
---
+++ src/main/java/com/codecool/tripplanner/moduls/Location.java
@@ -0,0 +1,12 @@
+package com.codecool.tripplanner.moduls;
+
+import com.codecool.tripplanner.enums.CityName;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Location {
+
+    private List<Movie> movies = new ArrayList<>();
+    private CityName cityName;
+}
