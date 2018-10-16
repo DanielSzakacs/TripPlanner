25deb6285861e443231f489fa98387582f02e230
danielszakacs <daniel.sokach@gmail.com>
2018-10-16 15:57:49 +0200

    Actor Entity is done

--- src/main/java/com/codecool/tripplanner/moduls/Movie.java
+++ src/main/java/com/codecool/tripplanner/moduls/Movie.java
@@ -2,10 +2,14 @@ package com.codecool.tripplanner.moduls;
 
 import com.codecool.tripplanner.enums.Genre;
 
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import java.util.ArrayList;
 import java.util.List;
 
 public class Movie {
+
 
     private List<Actor> actors = new ArrayList<>();
     private List<Location> locations = new ArrayList<>();
