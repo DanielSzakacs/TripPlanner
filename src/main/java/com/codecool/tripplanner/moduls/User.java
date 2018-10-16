2d6c33b1fbee792728a44f7b92d429f1dba66fa6
danielszakacs <daniel.sokach@gmail.com>
2018-10-16 15:36:49 +0200

    user and timeslot entity is done

--- src/main/java/com/codecool/tripplanner/moduls/User.java
+++ src/main/java/com/codecool/tripplanner/moduls/User.java
@@ -1,13 +1,42 @@
 package com.codecool.tripplanner.moduls;
 
+import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
 
-    private List<WalkingTour> walkingTourList = new ArrayList<>();
+    @OneToMany
     private List<Timeslot> timeslotList = new ArrayList<>();
+    @Column(name = "name")
     private String name;
+    @Column(name = "password")
     private String password;
 
+
+    public User(List<Timeslot> timeslotList, String name, String password) {
+        this.timeslotList = timeslotList;
+        this.name = name;
+        this.password = password;
+    }
+
+    public User(String name, String password) {
+        this.name = name;
+        this.password = password;
+    }
+
+    public User() {
+    }
+
+    public void addToTimeSlot(Timeslot timeslot){
+        this.timeslotList.add(timeslot);
+    }
+
+    public void addTimeSlot(List<Timeslot> timeslotList){
+        this.timeslotList = timeslotList;
+    }
 }
