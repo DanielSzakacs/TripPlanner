020f19f3e5abc33a2123fdd691eeae862e518ed1
danielszakacs <daniel.sokach@gmail.com>
2018-10-17 11:44:48 +0200

    pom file is updated, testing servlet is ready for test

---
+++ src/main/java/com/codecool/tripplanner/servlets/TestiPage.java
@@ -0,0 +1,41 @@
+package com.codecool.tripplanner.servlets;
+
+import com.codecool.tripplanner.config.TemplateEngineUtil;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.WebContext;
+
+import javax.json.Json;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebInitParam;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+@WebServlet(name = "TestingPage", urlPatterns = {"/"})
+@MultipartConfig
+public class TestiPage extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+        TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
+        WebContext context = new WebContext(request, response, request.getServletContext());
+
+        String json = Json.createObjectBuilder()
+                .add("key1", "value1")
+                .add("key2", "value2")
+                .build()
+                .toString();
+
+        System.out.println(json);
+        OutputStream os = response.getOutputStream();
+        os.write(json.getBytes());
+        os.close();
+
+//        context.setVariable("user", json);
+//        engine.process("myaccount.html", context, response.getWriter());
+    }
+}
