93914763f9e37391f00f3ff234b4b250f7d9c2aa
“elmerzsolt” <drogentm@gmail.com>
2018-10-17 15:19:34 +0200

    Basic frontend created.

--- src/main/java/com/codecool/tripplanner/JPA.java
+++ src/main/java/com/codecool/tripplanner/JPA.java
@@ -1,72 +1,81 @@
 package com.codecool.tripplanner;
 
-import com.codecool.tripplanner.*;
+import com.codecool.tripplanner.enums.CityName;
+import com.codecool.tripplanner.enums.Genre;
+import com.codecool.tripplanner.moduls.*;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
-import java.text.ParseException;
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
 import java.util.*;
 
 public class JPA {
 
+
+    private static EntityManager entityManager;
+    private static EntityManagerFactory entityManagerFactory;
+
+    private JPA() {
+        entityManagerFactory = Persistence.createEntityManagerFactory("jpaexamplePU");
+        entityManager =entityManagerFactory.createEntityManager();
+    }
+
     public static void populateDb(EntityManager em) {
-//        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
-//        Date birthDate1 = Calendar.getInstance().getTime();
-//        Date birthDate2 = Calendar.getInstance().getTime();
-//        try {
-//            birthDate1 = sdf.parse("1997-07-21");
-//            birthDate2 = sdf.parse("1993-12-01");
-//        } catch (ParseException e) {
-//            e.printStackTrace();
-//        }
-//
-//        List<String> phoneNumbers = new ArrayList<>(Arrays.asList("2d2","dsd"));
-//        Klass classBp2 = new Klass("Budapest 2016-2", CCLocation.BUDAPEST);
-//        Address address = new Address("Hungary", "1234", "Budapest", "Macskakő út 5.");
-//        Student student = new Student("Ödön", "odon@tokodon.hu", birthDate1, address, phoneNumbers);
-//        classBp2.addStudent(student);
-//
-//        EntityTransaction transaction = em.getTransaction();
-//        transaction.begin();
-//        em.persist(address);
-//        em.persist(student);
-//        transaction.commit();
-//        System.out.println("\n### Ödön saved.\n");
-//
-//        Address address2 = new Address("Hungary", "6789", "Budapest", "Harap u. 3.");
-//        Student student2 = new Student("Aladár", "ktyfl@gmail.com", birthDate2, address2, phoneNumbers);
-//        classBp2.addStudent(student2);
-//
-//        transaction.begin();
-//        em.persist(student2);
-//        em.persist(address2);
-//        em.persist(classBp2);
-//        transaction.commit();
-//        System.out.println("\n### Aladár saved.\n");
-//        em.remove(classBp2); // THis is to remove the class from the DB
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
+
+        Actor actor1 = new Actor("Miki");
+        Movie movie = new Movie("Code is life", Genre.ACTION);
+        movie.addActor(actor1);
+        actor1.addMovieToList(movie);
+
+        Location location = new Location(CityName.BUDAPEST);
+        location.addMovie(movie);
+
+        Timeslot timeslot = new Timeslot(LocalDateTime.now(), 5);
+        WalkingTour walkingTour = new WalkingTour(300, "Itt most van valami a vizben");
+        timeslot.addWalkingTour(walkingTour);
+        walkingTour.addTimeslot(timeslot);
+        walkingTour.addMovie(movie);
+
+        TripUser user = new TripUser("Timi", "password");
+        user.addToTimeSlot(timeslot);
+
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(actor1);
+        em.persist(movie);
+        em.persist(location);
+        em.persist(timeslot);
+        em.persist(walkingTour);
+        em.persist(user);
+        transaction.commit();
+        System.out.println("Commitolva lett");
+    }
+
+    public static EntityManager getInstance(){
+        if (entityManager == null){
+            new JPA();
+        }
+        return entityManager;
+
     }
 
 
     public static void main(String[] args) {
 
-        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpaexamplePU");
-        EntityManager em = emf.createEntityManager();
-
+        EntityManager em = getInstance();
         populateDb(em);
-        em.clear(); //clear hibernate cache - force next statements to read data from db
-
-
-
-        List<String> result = em.createNamedQuery("Student.findStudentsStartingWithChar").getResultList();
-        for (String name: result) {
-            System.out.println(name);
-        }
-
+        em.clear();
 
         em.close();
-        emf.close();
+        entityManagerFactory.close();
     }
+
+
 }
