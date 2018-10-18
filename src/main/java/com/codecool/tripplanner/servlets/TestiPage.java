6b9d170d07e020a1fd0ebcc0357105efc6b9c4bc
danielszakacs <daniel.sokach@gmail.com>
2018-10-18 11:11:49 +0200

    testing json .

--- src/main/java/com/codecool/tripplanner/servlets/TestiPage.java
+++ src/main/java/com/codecool/tripplanner/servlets/TestiPage.java
@@ -5,14 +5,12 @@ import com.codecool.tripplanner.config.TemplateEngineUtil;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
-import javax.json.Json;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.OutputStream;
 
 @WebServlet(name = "TestingPage", urlPatterns = {"/"})
 @MultipartConfig
@@ -24,33 +22,7 @@ public class TestiPage extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
 
-        String json = Json.createObjectBuilder()
-                .add("key1", "value1")
-                .add("key2", "value2")
-                .build()
-                .toString();
-
-
-
-//        JSONObject item = new JSONObject();
-//        JSONArray jsonArray = new JSONArray();
-//        try {
-//            item.put("name", "filename.java");
-//            item.put("filepath","aimaginarypath");
-//            jsonArray.put(item);
-//        } catch (JSONException e) {
-//            e.printStackTrace();
-//        }
-
-//        OutputStream os = response.getOutputStream();
-//        os.write(json.getBytes());
-//        os.close();
 
        engine.process("index.html", context, response.getWriter());
-
-        //engine.process("agency/index.html", context, response.getWriter());
-
-//        context.setVariable("user", json);
-//        engine.process("myaccount.html", context, response.getWriter());
     }
 }
