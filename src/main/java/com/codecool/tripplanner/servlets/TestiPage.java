eee8c3ff476de0d192f94fe94f6ea099edb7c42b
davidlenner <david910128@gmail.com>
2018-10-18 16:27:47 +0200

    frontend branch merged to servletside branch

--- src/main/java/com/codecool/tripplanner/servlets/TestiPage.java
+++ src/main/java/com/codecool/tripplanner/servlets/TestiPage.java
@@ -2,15 +2,19 @@ package com.codecool.tripplanner.servlets;
 
 import com.codecool.tripplanner.JPA;
 import com.codecool.tripplanner.config.TemplateEngineUtil;
+import com.codecool.tripplanner.moduls.*;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.WebContext;
 
+import javax.persistence.EntityManager;
 import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
+
 
 @WebServlet(name = "TestingPage", urlPatterns = {"/"})
 @MultipartConfig
@@ -22,8 +26,21 @@ public class TestiPage extends HttpServlet {
         TemplateEngine engine = TemplateEngineUtil.getTemplateEngine(request.getServletContext());
         WebContext context = new WebContext(request, response, request.getServletContext());
 
+        EntityManager em = JPA.getEntityManager();
+        List<WalkingTour> tours = em.createNamedQuery("displayalltour").getResultList();
+        List<Timeslot> timeslots = em.createNamedQuery("getalltimeslot").getResultList();
+        List<Movie> movies = em.createNamedQuery("getallmovies").getResultList();
+        List<Location> locations = em.createNamedQuery("getalllocations").getResultList();
+        List<Actor> actors = em.createNamedQuery("getallactors").getResultList();
+
+        context.setVariable("tours",tours);
+        context.setVariable("locations",locations);
+
 
        engine.process("agency/index.html", context, response.getWriter());
+
+        em.clear();
+        em.close();
     }
 
     @Override
