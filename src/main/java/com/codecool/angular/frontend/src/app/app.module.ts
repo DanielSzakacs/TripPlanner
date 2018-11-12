88e38896a572c99ba1f0733f0b4db9ef1f7f656c
danielszakacs <daniel.sokach@gmail.com>
2018-11-12 15:05:43 +0200

    Angular main page basis is done

--- src/main/java/com/codecool/angular/frontend/src/app/app.module.ts
+++ src/main/java/com/codecool/angular/frontend/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule} from "@angular/forms";
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
