7b5ced6da6d0fa0f92e7afe0c5bda7c086270164
danielszakacs <daniel.sokach@gmail.com>
2018-11-12 15:09:25 +0200

    Angular main page img css is added

---
+++ src/main/java/com/codecool/angular/frontend/src/app/main-page/main-page.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+@Component({
+  selector: 'app-main-page',
+  templateUrl: './main-page.component.html',
+  styleUrls: ['./static/css/bootstrap/bootstrap.min.css', './static/css/others/pe-icon-7-stroke.css', './static/css/others/magnific-popup.css', './static/css/others/animate.css',
+  './static/css/responsive/responsive.css']
+})
+export class MainPageComponent implements OnInit {
+  logo = './static/img/core-img/logo.png';
+
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+  }
+
+  search(data){
+    this.http.post('http://localhost:8088/data', {
+        data
+      }
+    ).subscribe(
+      res => {
+        console.log('Passing the json is done');
+      },
+      err => {
+        console.log('Sorry but something went wrong!');
+      }
+    );
+  }
+
+}
