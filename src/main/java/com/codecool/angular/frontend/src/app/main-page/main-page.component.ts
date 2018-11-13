f5b8360b003021f56091ad7d10bdec5e7989e289
danielszakacs <daniel.sokach@gmail.com>
2018-11-13 11:04:14 +0100

    Angular json get method is writen, need to test

--- src/main/java/com/codecool/angular/frontend/src/app/main-page/main-page.component.ts
+++ src/main/java/com/codecool/angular/frontend/src/app/main-page/main-page.component.ts
@@ -1,28 +1,42 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./static/css/bootstrap/bootstrap.min.css', './static/css/others/pe-icon-7-stroke.css', './static/css/others/magnific-popup.css', './static/css/others/animate.css',
-  './static/css/responsive/responsive.css', './main-page.component.css']
+    './static/css/responsive/responsive.css', './main-page.component.css']
 })
 export class MainPageComponent implements OnInit {
-  logo = '/static/img/core-img/logo.png';
+  result;
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+  }
 
   ngOnInit() {
   }
 
-  search(data){
+  search(data) {
     this.http.post('http://localhost:8088/data', {data}
     ).subscribe(
       res => {
         console.log('Passing the json is done');
+        this.getResults();
       },
       err => {
         console.log('Sorry but something went wrong!');
+      }
+    );
+  }
+ //This is the part when you get the searched information via json.
+  getResults() {
+    this.http.get("http://localhost:8088/data").subscribe(
+      res => {
+        console.log(res);
+        this.result = res;
+      },
+      err =>{
+        alert('Your JSON is not working');
       }
     );
   }
