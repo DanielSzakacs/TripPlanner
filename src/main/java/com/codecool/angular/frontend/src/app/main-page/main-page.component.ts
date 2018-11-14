9131fa5326b15eabebfaccb59474a2b00ebdd1be
danielszakacs <daniel.sokach@gmail.com>
2018-11-14 14:39:38 +0100

    Angular background color, style is done.

--- src/main/java/com/codecool/angular/frontend/src/app/main-page/main-page.component.ts
+++ src/main/java/com/codecool/angular/frontend/src/app/main-page/main-page.component.ts
@@ -1,6 +1,5 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-
 
 @Component({
   selector: 'app-main-page',
@@ -8,9 +7,11 @@ import {HttpClient} from "@angular/common/http";
   styleUrls: ['./static/css/bootstrap/bootstrap.min.css', './static/css/others/pe-icon-7-stroke.css', './static/css/others/magnific-popup.css', './static/css/others/animate.css',
     './static/css/responsive/responsive.css', './main-page.component.css']
 })
+
 export class MainPageComponent implements OnInit {
   result;
-  name = 'Name';
+  name: String;
+  animal: String;
 
   constructor(private http: HttpClient) {
   }
@@ -37,8 +38,5 @@ export class MainPageComponent implements OnInit {
     this.http.get('http://localhost:8088/data').subscribe((data) => this.result = data, error1 => {console.log(error1)});
     console.log(this.result);
   }
-
-
-
 
 }
