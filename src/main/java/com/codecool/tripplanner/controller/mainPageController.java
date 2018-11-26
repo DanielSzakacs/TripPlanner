baa63f4c51557fb080f04aca37830e2d71bbeba4
danielszakacs <daniel.sokach@gmail.com>
2018-11-26 14:57:30 +0100

    Spring tested. First repository query written

---
+++ src/main/java/com/codecool/tripplanner/controller/mainPageController.java
@@ -0,0 +1,21 @@
+package com.codecool.tripplanner.controller;
+
+import com.codecool.tripplanner.module.WalkingTour;
+import com.codecool.tripplanner.repository.WalkingTourRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class mainPageController {
+
+    @Autowired
+    private WalkingTourRepo walkingTourRepo;
+
+    @GetMapping("/data")
+    public List<WalkingTour> findAll(){
+        return walkingTourRepo.findAll();
+    }
+}
