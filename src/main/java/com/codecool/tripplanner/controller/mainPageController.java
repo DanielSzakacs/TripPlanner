f6f0420bd28e896e7cd0f1d6a62e9006680195c6
danielszakacs <daniel.sokach@gmail.com>
2018-11-26 15:55:41 +0100

    Angular Spring working together

--- src/main/java/com/codecool/tripplanner/controller/mainPageController.java
+++ src/main/java/com/codecool/tripplanner/controller/mainPageController.java
@@ -3,11 +3,13 @@ package com.codecool.tripplanner.controller;
 import com.codecool.tripplanner.module.WalkingTour;
 import com.codecool.tripplanner.repository.WalkingTourRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@CrossOrigin
 @RestController
 public class mainPageController {
 
