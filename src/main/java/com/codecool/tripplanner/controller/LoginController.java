c607706bc935e734ec2a09e26a9cb9c38cfabfb8
timardaniel <daniel.timar21@gmail.com>
2018-11-27 11:59:53 +0100

    get and parse login datas with Gson

---
+++ src/main/java/com/codecool/tripplanner/controller/LoginController.java
@@ -0,0 +1,49 @@
+package com.codecool.tripplanner.controller;
+
+import com.codecool.tripplanner.repository.TripUserRepo;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+
+import java.lang.reflect.Type;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.util.Map;
+
+@RestController
+@CrossOrigin
+public class LoginController {
+
+    private TripUserRepo tripUserRepo;
+
+    public LoginController(TripUserRepo tripUserRepo){
+        this.tripUserRepo = tripUserRepo;
+    }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String login(HttpServletRequest request) throws IOException{
+        HttpSession session = request.getSession();
+
+        // Specifies the time, in seconds, between client requests
+        // before the servlet container will invalidate this session.
+        session.setMaxInactiveInterval(120 * 60);
+
+        //https://stackoverflow.com/questions/43117731/what-is-type-typetoken
+        Gson gson = new Gson();
+        Type type = new TypeToken<Map<String, String>>() { }.getType();
+        Map<String, String> logindata = gson.fromJson(request.getReader() , type);
+
+
+
+
+        return null;
+    }
+
+
+}
