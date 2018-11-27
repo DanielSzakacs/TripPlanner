ffed775e367f058c2a8f983257f387e5e0125d88
danielszakacs <daniel.sokach@gmail.com>
2018-11-27 15:46:34 +0100

    search bugs are fixed

--- src/main/java/com/codecool/tripplanner/controller/mainPageController.java
+++ src/main/java/com/codecool/tripplanner/controller/mainPageController.java
@@ -9,10 +9,7 @@ import com.google.gson.reflect.TypeToken;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
@@ -32,26 +29,15 @@ public class mainPageController {
         return walkingTourRepo.findAll();
     }
 
-    @PostMapping("/search")
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
     public List<WalkingTour> search(HttpServletRequest request) throws IOException {
-        System.out.println("This is the request " + request);
-        System.out.println("THIS IIIIII " + request.getInputStream().toString());
-
         Gson gson = new Gson();
-        Type type = new TypeToken<Map<String, String>>() {
-        }.getType();
-        System.out.println(type.toString());
+        Type type = new TypeToken<Map<String, String>>() {}.getType();
         Map<String, String> searchdata = gson.fromJson(request.getReader(), type);
-        System.out.println(searchdata.get("city"));
-        System.out.println(searchdata.get("genre"));
 
-        System.out.println("CITYYY " + searchdata.get("city"));
-        System.out.println("GENREEEE " + searchdata.get("genre"));
-        //CityName cityName = CityName.valueOf(city);
-        //Genre genre1 = Genre.valueOf(genre);
-        //return walkingTourRepo.findAllByCityNameAndMoviegenre(cityName,genre1);
-        return null;
-
+        CityName cityName = CityName.valueOf(searchdata.get("city"));
+        Genre genre1 = Genre.valueOf(searchdata.get("genre"));
+        return walkingTourRepo.findAllByCityNameAndMoviegenre(cityName,genre1);
     }
 
 }
