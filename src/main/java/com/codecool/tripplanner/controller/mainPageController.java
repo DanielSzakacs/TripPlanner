7fd5ea9bf4959e33d9432a3f1a23e3265bddbba9
davidlenner <david910128@gmail.com>
2018-11-27 14:32:09 +0100

    get data from angular

--- src/main/java/com/codecool/tripplanner/controller/mainPageController.java
+++ src/main/java/com/codecool/tripplanner/controller/mainPageController.java
@@ -4,17 +4,21 @@ import com.codecool.tripplanner.enums.CityName;
 import com.codecool.tripplanner.enums.Genre;
 import com.codecool.tripplanner.module.WalkingTour;
 import com.codecool.tripplanner.repository.WalkingTourRepo;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
+import java.util.Map;
 
 @CrossOrigin
 @RestController
@@ -28,9 +32,17 @@ public class mainPageController {
         return walkingTourRepo.findAll();
     }
 
-    @PostMapping("/search")
-    public List<WalkingTour> search(HttpServletRequest request) {
+    @GetMapping("/search")
+    public List<WalkingTour> search(HttpServletRequest request) throws IOException {
         System.out.println(request);
+
+        Gson gson = new Gson();
+        Type type = new TypeToken<Map<String, String>>() {
+        }.getType();
+        Map<String, String> searchdata = gson.fromJson(request.getReader(), type);
+
+        System.out.println(searchdata.get("cityname"));
+        System.out.println(searchdata.get("genre"));
         //CityName cityName = CityName.valueOf(city);
         //Genre genre1 = Genre.valueOf(genre);
         //return walkingTourRepo.findAllByCityNameAndMoviegenre(cityName,genre1);
