9561935f1aa07fc4c634f1ccd28282559a0b8c47
davidlenner <david910128@gmail.com>
2018-11-29 15:30:31 +0100

    actorTest created

--- src/test/java/com/codecool/tripplanner/ActorTest.java
+++ src/test/java/com/codecool/tripplanner/ActorTest.java
@@ -5,32 +5,30 @@ import com.codecool.tripplanner.repository.ActorRepo;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@RunWith(SpringRunner.class)
+@RunWith(MockitoJUnitRunner.class)
 public class ActorTest {
-
 
     @Autowired
     ActorRepo actorRepo;
 
-    @Before
-    public void init(){
-
-        Actor budspencer = new Actor("Bud Spencer");
-        Mockito.when(actorRepo.getActorByName(budspencer.getName())).thenReturn(budspencer);
-    }
 
     @Test
     public void getActorByName(){
 
         String name = "Bud Spencer";
-        Actor actor = actorRepo.getActorByName(name);
 
-        assertThat(actor.getName()).isEqualTo(name);
+        Actor budspencer = new Actor("Bud Spencer");
+
+        Mockito.when(actorRepo.getActorByName(budspencer.getName())).thenReturn(budspencer);
+
+        assertThat(actorRepo.getActorByName(name)).isEqualTo(actorRepo.getActorByName("Bud Spencer"));
     }
 }
